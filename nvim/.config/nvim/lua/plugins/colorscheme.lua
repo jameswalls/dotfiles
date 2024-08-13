343195873e5d143b941e0f1434ee98b7c5d0fdd7
J. Jaime Paredes <35391305+jameswalls@users.noreply.github.com>
2024-08-12 19:44:39 -0600

    reomove italics catppuccin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -73,7 +73,7 @@ return {
 					shade = "dark",
 					percentage = 0.15, -- percentage of the shade to apply to the inactive window
 				},
-				no_italic = false, -- Force no italic
+				no_italic = true, -- Force no italic
 				no_bold = false, -- Force no bold
 				no_underline = false, -- Force no underline
 				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
