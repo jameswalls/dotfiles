5679571046385bc734e406802972be27a440561e
james walls <35391305+jameswalls@users.noreply.github.com>
2024-12-15 15:12:48 -0600

    nvim: enable treesitter context

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter-context",
-	enabled = false,
+	enabled = true,
 	config = function()
 		require("treesitter-context").setup({
 			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
@@ -8,7 +8,7 @@ return {
 			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 			line_numbers = true,
 			multiline_threshold = 1, -- Maximum number of lines to show for a single context
-			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: "inner", "outer"
+			trim_scope = "inner", -- Which context lines to discard if `max_lines` is exceeded. Choices: "inner", "outer"
 			mode = "topline",  -- Line used to calculate context. Choices: "cursor", "topline"
 			separator = "",
 			zindex = 20, -- The Z-index of the context window
