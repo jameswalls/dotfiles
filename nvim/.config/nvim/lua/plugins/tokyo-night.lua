0305e75326ef5b6563fff37057edfb51cd3ff7f9
james walls <35391305+jameswalls@users.noreply.github.com>
2024-12-22 19:40:57 -0600

    nvim: add tokyonight

---
+++ nvim/.config/nvim/lua/plugins/tokyo-night.lua
@@ -0,0 +1,30 @@
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			style = "storm",
+			transparent = true,
+			styles = {
+				comments = { italic = false },
+				keywords = { italic = false, bold=true },
+			},
+			on_highlights = function(hl, c)
+				hl.LineNrAbove = { fg=c.blue }
+				hl.LineNrBelow = { fg=c.blue }
+				hl.DiagnosticVirtualTextError = { fg=c.red }
+				hl.DiagnosticVirtualTextHint = { fg=c.green1 }
+				hl.TelescopeNormal = {}
+				hl.TelescopeBorder = {}
+				hl.TelescopePromptBorder = {}
+				hl.TelescopePromptTitle = { fg = c.orange }
+				hl.TelescopePreviewTitle = { fg = c.orange }
+				hl.TelescopePromptTitle = { fg = c.orange }
+				hl.TelescopeResultstitle = { fg = c.orange }
+			end,
+		})
+
+		vim.cmd.colorscheme("tokyonight")
+	end
+}
