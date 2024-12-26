e26d239b7f7b4145230c61a725c3b2933aed7fe2
James Walls <35391305+jameswalls@users.noreply.github.com>
2024-12-26 11:48:29 -0600

    nvim: catppuccin colorscheme

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -37,7 +37,17 @@ return {
 				-- miscs = {}, -- Uncomment to turn off hard-coded styles
 			},
 			color_overrides = {},
-			custom_highlights = {},
+			custom_highlights = function(colors)
+				return {
+					["@module"] = { style = {} },
+					TelescopeSelection = { bg = colors.surface1 },
+					DiagnosticVirtualTextError = { style = {} },
+					DiagnosticVirtualTextHint = { style = {} },
+					DiagnosticVirtualTextInfo = { style = {} },
+					DiagnosticVirtualTextOk = { style = {} },
+					DiagnosticVirtualTextWarn = { style = {} },
+				}
+			end,
 			default_integrations = true,
 			integrations = {
 				cmp = true,
