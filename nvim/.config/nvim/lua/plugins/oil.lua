01bcaed99b27e589dbdfd5530b13d958387e95da
Jaime Paredes <35391305+jameswalls@users.noreply.github.com>
2024-12-26 12:59:59 -0600

    nvim: oil floating

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -9,14 +9,35 @@ return {
 			view_options = {
 				natural_order = false,
 			},
+
+			float = {
+				-- Padding around the floating window
+				padding = 0,
+				max_width = 100,
+				max_height = 20,
+				border = "rounded",
+				win_options = {
+					winblend = 0,
+				},
+				-- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+				get_win_title = nil,
+				-- preview_split: Split direction: "auto", "left", "right", "above", "below".
+				preview_split = "below",
+				-- This is the config that will be passed to nvim_open_win.
+				-- Change values here to customize the layout
+				override = function(conf)
+					return conf
+				end,
+			},
 			keymaps = {
 				["<C-p>"] = function() oil.open_preview({ horizontal=true }) end,
 				["g."] = "actions.toggle_hidden",
 				["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
 				["<C-x>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+				["<Esc>"] = function() oil.toggle_float() end,
 			}
 		})
 
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "-", oil.toggle_float, { desc = "Open parent directory" })
 	end
 }
