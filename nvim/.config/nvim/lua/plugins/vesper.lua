8667fcbccac84d9cf587116341f28bea5e81923e
james walls <35391305+jameswalls@users.noreply.github.com>
2024-12-30 19:31:33 -0600

    nvim: change vesper repo

--- nvim/.config/nvim/lua/plugins/vesper.lua
+++ nvim/.config/nvim/lua/plugins/vesper.lua
@@ -1,37 +1,24 @@
 return {
-	"gambhirsharma/vesper.nvim",
+	"datsfilipe/vesper.nvim",
 	lazy = false,
 	priority = 1000,
-	name = "vesper",
 	config = function ()
-		local colors = require("vesper").palette
+		local colors = require("vesper.colors")
 		require("vesper").setup({
-			terminal_colors = true,
-			undercurl = true,
-			underline = true,
-			bold = true,
-			italic = {
-				strings = false,
-				emphasis = false,
+			transparent = true,
+			italics = {
 				comments = false,
-				operators = false,
-				folds = false,
+				keywords = false,
+				functions = false,
+				strings = false,
+				variables = false,
 			},
-			strikethrough = true,
-			invert_selection = false,
-			invert_signs = false,
-			invert_tabline = false,
-			invert_intend_guides = false,
-			inverse = true,
-			contrast = "",
 			palette_overrides = {},
 			overrides = {
 				CursorLineNr = { bg='none', bold=true },
-				LineNr = { fg=colors.gray },
-				Visual = { bg=colors.dark4 },
+				Conditional = { fg=colors.borderDarker, bold=true },
+				Statement = { fg=colors.borderDarker, bold=true },
 			},
-			dim_inactive = false,
-			transparent_mode = true,
 		})
 		vim.cmd.colorscheme("vesper")
 	end
