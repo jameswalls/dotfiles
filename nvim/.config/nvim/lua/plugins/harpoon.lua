021bed1afe0ab8b1352264ee7dd168710f9440d5
james walls <35391305+jameswalls@users.noreply.github.com>
2025-01-03 17:11:45 -0600

    nvim: harpoon autocmd

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -19,8 +19,25 @@ return {
 		vim.keymap.set("n", "<space>s", function() harpoon:list():select(2) end)
 		vim.keymap.set("n", "<space>d", function() harpoon:list():select(3) end)
 		vim.keymap.set("n", "<space>f", function() harpoon:list():select(4) end)
-		vim.keymap.set("n", "<space>g", function() harpoon:list():select(5) end)
+		vim.keymap.set("n", "<space>z", function()
+			local idx = harpoon:list()["_index"]
+			harpoon:list():select(idx)
+		end)
 		vim.keymap.set("n", "<leader>p", function() harpoon:list():prev(nav_opts) end)
 		vim.keymap.set("n", "<leader>n", function() harpoon:list():next(nav_opts) end)
+
+		vim.api.nvim_create_autocmd("BufEnter", {
+			group = "Harpoon",
+			callback = function()
+				local current_path = vim.fn.fnamemodify(vim.fn.expand('%'), ':p:~:.')
+				local current_list = harpoon:list()
+				for idx, item in ipairs(current_list["items"]) do
+					local path = item["value"]
+					if current_path == path then
+						current_list["_index"] = idx
+					end
+				end
+			end
+		})
 	end
 }
