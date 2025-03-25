01b4c3dca25b216a7e0b3fe2edde10a8832d7e4b
james walls <35391305+jameswalls@users.noreply.github.com>
2025-03-25 14:48:02 -0600

    nvim: remove window navigation commands... tmux interference

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,4 @@
 vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<leader>]", ":lnext <Cr>")
 vim.keymap.set("n", "<leader>[", ":lprev <Cr>")
 vim.keymap.set("n", "]q", ":cnext <Cr>")
