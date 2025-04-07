0b93b178151922f622d32c4ce4eb85f7625cbf67
james walls <35391305+jameswalls@users.noreply.github.com>
2025-04-07 02:16:40 -0600

    nvim: customize lacklluster

--- nvim/.config/nvim/lua/plugins/lackluster.lua
+++ nvim/.config/nvim/lua/plugins/lackluster.lua
@@ -3,10 +3,23 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function ()
+		local color = require("lackluster.color")
 		require("lackluster").setup({
+			tweak_syntax = {
+				comment = color.gray5,
+				keyword = color.gray6,
+				keyword_return = color.gray6,
+				keyword_exception = color.gray6,
+			},
 			tweak_background = {
 				normal = "none",
 				telescope = "none",
+			},
+			tweak_highlight = {
+				OilDir = { fg = color.blue },
+				["@keyword"] = { bold = true },
+				["@keyword.exception"] = { bold = true },
+				["@keyword.return"] = { bold = true }
 			}
 		})
 		vim.cmd.colorscheme("lackluster")
