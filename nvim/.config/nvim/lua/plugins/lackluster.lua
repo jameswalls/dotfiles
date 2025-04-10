bf754c99520fa6d0dfe9d6b9e56fef9a057bc17d
james walls <35391305+jameswalls@users.noreply.github.com>
2025-04-10 16:27:04 -0600

    lackluster bright colors

--- nvim/.config/nvim/lua/plugins/lackluster.lua
+++ nvim/.config/nvim/lua/plugins/lackluster.lua
@@ -1,8 +1,22 @@
+local function brighten_hex(hex, amount)
+    hex = hex:gsub("#","")
+    local r = tonumber("0x"..hex:sub(1,2))
+    local g = tonumber("0x"..hex:sub(3,4))
+    local b = tonumber("0x"..hex:sub(5,6))
+
+    r = math.min(255, r + amount)
+    g = math.min(255, g + amount)
+    b = math.min(255, b + amount)
+
+    return string.format("#%02x%02x%02x", r, g, b)
+end
+
 return {
 	"slugbyte/lackluster.nvim",
-	lazy = false,
+	lazy = true,
 	priority = 1000,
 	config = function ()
+
 		local color = require("lackluster.color")
 		require("lackluster").setup({
 			tweak_syntax = {
@@ -15,14 +29,35 @@ return {
 				normal = "none",
 				telescope = "none",
 			},
+			tweak_color = {
+				lack   = brighten_hex(color.blue, 20),
+				luster = brighten_hex(color.luster, 20),
+				orange = brighten_hex(color.orange, 20),
+				yellow = brighten_hex(color.yellow, 20),
+				green  = brighten_hex(color.green, 20),
+				blue   = brighten_hex(color.blue, 20),
+				red    = brighten_hex(color.red, 20),
+				black  = brighten_hex(color.black, 10),
+				gray1  = brighten_hex(color.gray1, 10),
+				gray2  = brighten_hex(color.gray2, 10),
+				gray3  = brighten_hex(color.gray3, 10),
+				gray4  = brighten_hex(color.gray4, 10),
+				gray5  = brighten_hex(color.gray5, 10),
+				gray6  = brighten_hex(color.gray6, 10),
+				gray7  = brighten_hex(color.gray7, 10),
+				gray8  = brighten_hex(color.gray8, 10),
+				gray9  = brighten_hex(color.gray9, 10),
+			},
 			tweak_highlight = {
 				OilDir = { fg = color.gray7 },
 				["@property.json"] = { fg = color.gray7 },
 				["@keyword"] = { bold = true },
 				["@keyword.exception"] = { bold = true },
-				["@keyword.return"] = { bold = true }
+				["@keyword.return"] = { bold = true },
+				["TelescopeMatching"] = { bold = true },
 			}
 		})
+		print(brighten_hex(color.gray9, 10))
 		vim.cmd.colorscheme("lackluster")
 	end
 }
