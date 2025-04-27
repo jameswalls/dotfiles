6484d61a7ca3a18c73476ac50e22a9f1948b384e
james walls <35391305+jameswalls@users.noreply.github.com>
2025-04-27 03:31:54 -0600

    nvim: move autocommands to separate file

---
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -0,0 +1,25 @@
+local function setup_autocmds()
+
+	vim.api.nvim_create_autocmd("TextYankPost", {
+		desc = "Highlight when yanking (copying) text",
+		group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+		callback = function()
+			vim.highlight.on_yank({ higroup="Visual" })
+		end,
+	})
+
+	-- Set tab size for Lua files
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = { "lua", "markdown" },
+		callback = function()
+			vim.opt.tabstop = 2
+			vim.opt.shiftwidth = 2
+			vim.opt.expandtab = true
+		end,
+	})
+
+end
+
+return {
+   setup = setup_autocmds
+}
