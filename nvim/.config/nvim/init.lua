487e341bf0bde3187f614d07a6a8cd1bd50391ae
james walls <35391305+jameswalls@users.noreply.github.com>
2025-05-05 00:12:50 -0600

    nvim: background

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -36,7 +36,7 @@ vim.opt.cursorline = true
 vim.o.termguicolors = true
 
 vim.cmd.colorscheme("lunaperche")
--- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Normal', { bg='none', fg="#c6c6c6" })
 vim.api.nvim_set_hl(0, 'Cursor', { reverse=true })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg='none' })
 vim.api.nvim_set_hl(0, "Vertsplit", { fg="#c6c6c6"})
