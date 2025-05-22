4ce0b1759a7754f79e5d2de58b6ccc0eb1e244ab
james walls <35391305+jameswalls@users.noreply.github.com>
2025-05-22 15:21:38 -0600

    nvim: cyberdream highlights

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -4,7 +4,7 @@ local function setup_autocmds()
 		desc = "Highlight when yanking (copying) text",
 		group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
 		callback = function()
-			vim.highlight.on_yank({ higroup="Visual" })
+			vim.hl.on_yank({ higroup="Visual" })
 		end,
 	})
 
