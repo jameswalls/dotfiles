8d931e38e52847c77ef2dca3a841edae28230bed
james walls <35391305+jameswalls@users.noreply.github.com>
2025-06-05 17:08:03 -0600

    nvim: neomodern

---
+++ nvim/.config/nvim/lua/plugins/neomodern.lua
@@ -0,0 +1,41 @@
+return {
+  "cdmill/neomodern.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function ()
+    require("neomodern").setup({
+      theme = "iceclimber",
+      variant = "dark",
+      alt_bg = true,
+      colored_docstrings = false,
+      cursorline_gutter = true,
+      dark_gutter = false,
+      favor_treesitter_hl = false,
+      plain_float = false,
+      show_eob = true,
+      term_colors = true,
+      toggle_variant_key = nil,
+      transparent = true,
+      diagnostics = {
+        darker = true,
+        undercurl = true,
+        background = true,
+      },
+      code_style = {
+        comments = "none",
+        conditionals = "none",
+        functions = "none",
+        keywords = "bold",
+        headings = "bold", -- Markdown headings
+        operators = "none",
+        keyword_return = "bold",
+        strings = "none",
+        variables = "none",
+      },
+      highlights = {
+        FloatBorder = {fg="#555568", bg="none"}
+      }
+    })
+    require("neomodern").load()
+  end
+}
