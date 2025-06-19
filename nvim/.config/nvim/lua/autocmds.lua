07c7f29e93540fdc8d8e5c6e24bba960c5ac0059
james walls <35391305+jameswalls@users.noreply.github.com>
2025-06-18 23:56:32 -0600

    nvim: conform

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -25,6 +25,12 @@ local function setup_autocmds()
     end,
   })
 
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+      require("conform").format({ bufnr = args.buf })
+    end,
+  })
 end
 
 return {
