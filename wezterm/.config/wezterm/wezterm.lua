d01479e0ca9f91aafe63ee50940841cf91dccc34
james walls <35391305+jameswalls@users.noreply.github.com>
2025-06-22 16:31:56 -0600

    wezterm: opacity

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -169,7 +169,7 @@ config.window_decorations = "RESIZE|MACOS_FORCE_DISABLE_SHADOW"
 config.window_close_confirmation = "NeverPrompt"
 config.force_reverse_video_cursor = true
 config.bold_brightens_ansi_colors = false
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.80
 config.macos_window_background_blur = 0
 config.keys = {
 	{
