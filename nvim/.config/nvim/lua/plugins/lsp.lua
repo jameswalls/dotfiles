2334391b8461fa8b9b965f45332c37c8ab2637dc
James <35391305+jameswalls@users.noreply.github.com>
2025-07-31 17:58:42 -0600

    nvim: format lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -11,14 +11,14 @@ return {
 					suppress_on_insert = true,
 					display = {
 						render_limit = 1,
-					}
+					},
 				},
 				notification = {
 					window = {
-						winblend = 0
-					}
-				}
-			}
+						winblend = 0,
+					},
+				},
+			},
 		},
 	},
 	config = function()
@@ -28,23 +28,34 @@ return {
 				local map = function(keys, func, desc)
 					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
-        local window_opts = { border = "rounded" }
+				local window_opts = { border = "rounded" }
 				local telescope = require("telescope.builtin")
 				map("gd", telescope.lsp_definitions, "[G]oto [D]efinition")
 				map("gr", telescope.lsp_references, "[G]oto [R]eferences")
 				map("gI", telescope.lsp_implementations, "[G]oto [I]mplementation")
-				map("<leader>ds", function() telescope.lsp_document_symbols({symbol_width=60}) end, "[D]ocument [S]ymbols")
+				map("<leader>ds", function()
+					telescope.lsp_document_symbols({ symbol_width = 60 })
+				end, "[D]ocument [S]ymbols")
 				map("<leader>ws", telescope.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 				map("<leader>td", telescope.lsp_type_definitions, "[T]ype [D]efinitions")
 				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 				map("<leader>ga", vim.lsp.buf.code_action, "[G]et code [a]ctions")
-				map("K", function() vim.lsp.buf.hover(window_opts) end , "Hover Documentation")
-				map("H", function() vim.lsp.buf.signature_help(window_opts) end, "Signature [H]elp")
+				map("K", function()
+					vim.lsp.buf.hover(window_opts)
+				end, "Hover Documentation")
+				map("H", function()
+					vim.lsp.buf.signature_help(window_opts)
+				end, "Signature [H]elp")
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-				map("<leader>e", function() vim.diagnostic.open_float(window_opts) end, "Diagnostic float")
-				map("[d", function() vim.diagnostic.jump({ count = -1, float = window_opts }) end, "Go to previous [D]iagnostic message")
-				map("]d", function() vim.diagnostic.jump({ count = 1, float = window_opts }) end, "Go to next [D]iagnostic message")
-
+				map("<leader>e", function()
+					vim.diagnostic.open_float(window_opts)
+				end, "Diagnostic float")
+				map("[d", function()
+					vim.diagnostic.jump({ count = -1, float = window_opts })
+				end, "Go to previous [D]iagnostic message")
+				map("]d", function()
+					vim.diagnostic.jump({ count = 1, float = window_opts })
+				end, "Go to next [D]iagnostic message")
 			end,
 		})
 
@@ -53,71 +64,71 @@ return {
 
 		local servers = {
 			pylsp = {
-        enabled = false,
+				enabled = false,
 				filetypes = { "python" },
 				settings = {
 					pylsp = {
-            configuratinSources = { "pycodestyle", "pyflakes" },
+						configuratinSources = { "pycodestyle", "pyflakes" },
 						plugins = {
-              pyflakes = {
-                enabled = false,
-              },
-              pycodestyle = {
-                enabled = true,
-                maxLineLength = 120,
-              },
+							pyflakes = {
+								enabled = false,
+							},
+							pycodestyle = {
+								enabled = true,
+								maxLineLength = 120,
+							},
 							jedi_completion = {
 								enabled = true,
 								include_function_objects = false,
 								include_class_objects = false,
 							},
-						}
-					}
-				}
+						},
+					},
+				},
 			},
-      pyright = {
-        enabled = true,
+			pyright = {
+				enabled = true,
 				filetypes = { "python" },
-				settings = {}
+				settings = {},
 			},
 			rust_analyzer = {
-        enabled = true,
-        filetypes = { "rust" },
-        settings = {
-          ["rust-analyzer"] = {
-            rustfmt = {
-              enabled = true
-            }
-          }
-        }
+				enabled = true,
+				filetypes = { "rust" },
+				settings = {
+					["rust-analyzer"] = {
+						rustfmt = {
+							enabled = true,
+						},
+					},
+				},
 			},
 		}
 
-    require("mason").setup({
-      registries = {
-        "github:mason-org/mason-registry",
-      }
-    })
+		require("mason").setup({
+			registries = {
+				"github:mason-org/mason-registry",
+			},
+		})
 
 		local ensure_installed = vim.tbl_keys(servers or {})
-    local exclude = {}
-    for server, settings in pairs(servers) do
-      if not settings["enabled"] then
-        table.insert(exclude, server)
-      end
-    end
+		local exclude = {}
+		for server, settings in pairs(servers) do
+			if not settings["enabled"] then
+				table.insert(exclude, server)
+			end
+		end
 
-    for server, settings in pairs(servers) do
-      if settings["enabled"] then
-        vim.lsp.config(server, settings)
-      end
-    end
+		for server, settings in pairs(servers) do
+			if settings["enabled"] then
+				vim.lsp.config(server, settings)
+			end
+		end
 
 		require("mason-lspconfig").setup({
-      ensure_installed = ensure_installed,
-      automatic_enable = {
-        exclude = exclude
-      },
+			ensure_installed = ensure_installed,
+			automatic_enable = {
+				exclude = exclude,
+			},
 		})
-	end
+	end,
 }
