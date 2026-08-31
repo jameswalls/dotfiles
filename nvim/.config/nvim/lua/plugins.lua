d78e0c56bbc9f01b17147e416560f590ddeef907
jwls <35391305+jwllzp@users.noreply.github.com>
2026-08-30 21:16:19 -0600

    nvim: cyberdream

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -14,5 +14,5 @@ require("plugins.telescope")
 require("plugins.treesitter")
 
 -- colorscheme must go at the end
-require("plugins.themes.neomodern")
+require("plugins.themes.cyberdream")
 require("highlights")
